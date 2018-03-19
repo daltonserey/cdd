3789d219981ef43b1ae09d27ab351968b8f62530
Dalton Serey <daltonserey@gmail.com>
2018-03-18 22:03:00 -0300

    Fix README.md in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=['cdd',],
     license='GNU Affero General Public License v3',
     description='improved file system navigation with cd',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     url='https://github.com/daltonserey/cdd',
     author='Dalton Serey',
     author_email='daltonserey@gmail.com',
